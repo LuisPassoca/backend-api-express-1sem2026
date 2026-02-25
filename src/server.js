cbacb544306a07b2daa57842b493a0ae19f82c4a
LuisPassoca <luiseduardopmagalhaes@gmail.com>
2026-02-24 22:15:15 -0300

    Adding new routes

--- src/server.js
+++ src/server.js
@@ -1,9 +1,25 @@
-const express = require('express')
+import express from "express"
 const app = express()
 const PORT = 3000
 
 app.get('/', (req, res) => {
     res.send('Olá mundo!')
+})
+
+app.post('/users', (req, res) => {
+    res.send('POST na rota /users')
+})
+
+app.put('/users', (req, res) => {
+    res.send('PUT na rota /users')
+})
+
+app.patch('/users', (req, res) => {
+    res.send('PATCH na rota /users')
+})
+
+app.delete('/users', (req, res) => {
+    res.send('DELETE na rota /users')
 })
 
 app.listen(PORT, () => {
