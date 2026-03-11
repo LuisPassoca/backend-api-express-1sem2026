5a6c857e7e686b40d45eb03c8e472fd80d7cc31f
LuisPassoca <luiseduardopmagalhaes@gmail.com>
2026-03-11 14:47:52 -0300

    Set up prisma, database and added model createUser

--- src/controllers/user/createUserController.js
+++ src/controllers/user/createUserController.js
@@ -1,3 +1,18 @@
-export function createUserController(req, res) {
-    res.send('POST na rota /user')
+import { createUser } from "../../models/userModel.js";
+
+export async function createUserController(req, res) {
+    
+    const user = {
+        avatar: "https://github.com/luispassoca.png",
+        name: "Luis Passoca",
+        email: "luispassoca@sample.com",
+        pass: "açúcar"
+    }
+
+    const result = await createUser(user)
+
+    res.json({
+        message: `O usuário ${user.name} foi criado com sucesso!`,
+        user: result
+    })
 }
